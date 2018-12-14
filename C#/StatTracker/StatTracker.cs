fb5c6eb071fec9ecefc1fa6c44b7a2ec432f8e73
alexjvan <Dreamonclaids@gmail.com>
2018-12-13 20:53:14 -0800

    Update to Add in C# code

---
+++ C#/StatTracker/StatTracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTracker
+{
+    public class StatTracker
+    {
+        public string FileLocation = "";
+
+        public void NewUser(string username)
+        {
+            
+        }
+
+        public void AddStat(string username, string statName, float count)
+        {
+
+        }
+
+        public void PushUpdate()
+        {
+
+        }
+    }
+}
